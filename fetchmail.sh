67a71af1ab98b4c55647ecf80bd8b2721134cce0
sunny-logic <vk.sanmati@gmail.com>
2017-02-25 23:44:19 +0530

    Create fetchmail.sh

---
+++ fetchmail.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This shell script will be consigured in Cron to run at predefined time.
+# Please create a shell script in the mentioned path /home/osmc/email_jarvis/fetchmail.sh
+# https://github.com/sunny-logic/email_jarvis
+# Version 0.1
+
+fetchmail > /dev/null
